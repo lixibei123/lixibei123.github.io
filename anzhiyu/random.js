713ad67a2d89c3312a045f4b3980dad430fc4341
lixibei123 <19713017697@163.com>
2026-01-13 10:22:13 +0800

    Site updated: 2026-01-13 10:22:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3357.html","posts/2700.html","posts/3eeb.html","posts/3290.html","posts/b26f.html","posts/df2a.html","posts/337a.html","posts/10023.html","posts/ab5b.html","posts/c5be.html","posts/fb4c.html","posts/94a6.html"];function toRandomPost(){
+var posts=["posts/3357.html","posts/2700.html","posts/3290.html","posts/b26f.html","posts/df2a.html","posts/337a.html","posts/10023.html","posts/ab5b.html","posts/c5be.html","posts/94a6.html","posts/fb4c.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
